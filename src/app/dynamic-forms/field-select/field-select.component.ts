87d072b61ae381a99b1f0486e519216248a3dec5
michalkotas <miko@mikofoto.net>
2018-05-27 21:51:09 +0200

    add dependsOnField config option

--- src/app/dynamic-forms/field-select/field-select.component.ts
+++ src/app/dynamic-forms/field-select/field-select.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 import { SelectItem } from 'primeng/api';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-field-select',
@@ -15,12 +17,15 @@ export class FieldSelectComponent extends FieldType implements OnInit {
   }
 
   ngOnInit() {
-    // this.formControl.setValue('Volvo');
-    this.selectOptions = this.field.templateOptions.options as any[];
-    console.log('this', this.field.templateOptions.options);
-    this.formControl.valueChanges.subscribe(value => {
-      console.log('options', this);
-    });
+    if (this.field.templateOptions.dependsOnField) {
+      const parentFormControl: FormControl = this.form.controls[this.field.templateOptions.dependsOnField] as FormControl;
+      if (parentFormControl) {
+        parentFormControl.valueChanges.pipe(debounceTime(100)).subscribe(value => {
+          this.selectOptions = (this.field.templateOptions.options as any[]).filter(option => option.parentValue === value);
+        });
+      }
+    } else {
+      this.selectOptions = this.field.templateOptions.options as SelectItem[];
+    }
   }
-
 }
