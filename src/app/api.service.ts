26731c847468bb2fe155ffb67f4348e8badde300
michalkotas <miko@mikofoto.net>
2018-05-27 19:28:36 +0200

    add simple api for fields

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+
+const DATA = './assets/db.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getSchema(): Observable<any> {
+    return this.http
+      .get(DATA)
+      .pipe(map(resp => resp), catchError(this.handleError('getHeroes', [])));
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getSchema().subscribe((schema) => {
+      console.log(schema);
+    });
+  }
 }
